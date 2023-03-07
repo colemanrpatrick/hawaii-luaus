485f993c8562ca3c13678527a78190cee75c8535
patrick <17029142+colemanrpatrick@users.noreply.github.com>
2023-03-07 17:46:32 -0500

    major update

---
+++ js/precheckout.js
@@ -0,0 +1,329 @@
+console.log("precheckout.js");
+
+let page1 = document.getElementById("page-1");
+let page2 = document.getElementById("page-2");
+let page3 = document.getElementById("page-3");
+
+/*===========================================================*/
+/*==================| local/current date |==================*/
+/*=========================================================*/
+
+const getHawaiiTime = function() {
+	let hawaii_datetime_str = new Date().toLocaleString( "en-GB", {
+		timeZone: "Pacific/Honolulu"
+	}, {
+		hour12: false
+	} );
+	dateArr = hawaii_datetime_str.split( ",", 2 );
+	dateArr.shift();
+	hawaii_datetime_str = dateArr[ 0 ].slice( 1 );
+	hawaii_datetime_str = parseInt( hawaii_datetime_str );
+	return hawaii_datetime_str;
+};
+const getTodaysDate = function() {
+	let currentDate = new Date().toLocaleDateString( 'en-US', {
+		year: "numeric",
+		month: "2-digit",
+		day: "2-digit",
+	} );
+	return currentDate;
+};
+/*========================================================*/
+/*=================|   numberIncremet  |=================*/
+/*======================================================*/
+
+function numIncrement( numberInput, increase ) { 
+
+	var myInputObject = document.getElementById( numberInput ); 
+	if ( increase ) { 
+		myInputObject.value++;
+		localStorage.setItem( "" + myInputObject.getAttribute( "name" ) + "", myInputObject.value );
+		 
+	} else { 
+		myInputObject.value--;
+		localStorage.setItem( "" + myInputObject.getAttribute( "name" ) + "", myInputObject.value );
+		 
+	}; 
+	if ( myInputObject.value > 999 ) {
+		myInputObject.value = 999;
+	};
+	if ( myInputObject.value <= 0 ) {
+		myInputObject.value = 0;
+	};
+
+}; 
+
+/*========================================================*/
+/*==================|   Date Picker   |==================*/
+/*======================================================*/
+
+let createDatePicker = function(landing){
+
+	let dateInput = document.createElement("input");
+	let datePicker = document.createElement("div");
+	
+	dateInput.setAttribute("type","text");
+	dateInput.setAttribute("id","dateInput");
+	datePicker.setAttribute('id','datepicker');
+
+	landing.appendChild(dateInput);
+	landing.appendChild(datePicker);
+
+/*========== jQuery UI datepicker functions ==========*/
+
+	var dateToday = new Date();
+	
+	// list of specific disabled dates //
+	let disabledDates = cartConfig.Availabilities[0].ClosedDates;
+	disabledDates = JSON.parse(disabledDates);
+
+	for (var i = 0; i < disabledDates.length; i++) {
+		disabledDates[i] = disabledDates[i].replace(/\//g, '-');
+	};
+
+    $(datePicker).datepicker({
+        minDate: dateToday,// dates before current day disabled //
+        beforeShowDay: function (date) { // disables dates based on disabledDates
+            var disabledDatesString = jQuery.datepicker.formatDate('mm-dd-yy', date);
+            return [disabledDates.indexOf(disabledDatesString) == -1]
+        }
+    });
+
+	$("#dateInput").attr("name", "" + cartConfig.Collectors[0].ControlName + "");
+
+	/*====== set datepicker and input events ======*/
+	$("#dateInput").change(function () {
+        $("#datepicker").datepicker('setDate', $(this).val()).trigger('change');
+    });
+
+    $("#datepicker").change(function () {
+        if ($("#dateInput").val() !== disabledDates) {
+            $("#dateInput").prop('value', $(this).val());
+            localStorage.setItem("" + $('#dateInput').attr('name') + "", "" + $('#dateInput').val() + "");
+        };
+    });
+};
+/*========================================================*/
+/*==================|  number spinner |==================*/
+/*======================================================*/
+
+let createSpinners = function(landing,$name,index){
+
+        let numberSpinner = document.createElement("DIV");
+		numberSpinner.setAttribute("class","numberSpinner");
+
+        let spinnerSection = document.createElement("SECTION");
+
+		let spinnerInput = document.createElement('INPUT');
+		spinnerInput.setAttribute("type","number");
+		spinnerInput.setAttribute("name",$name);
+		spinnerInput.setAttribute("id",$name);
+		spinnerInput.setAttribute('class','price');
+		spinnerInput.setAttribute('min',0);
+
+        let spinnerPlus = document.createElement("BUTTON");
+        let spinnerMinus = document.createElement("BUTTON");
+        spinnerPlus.setAttribute("type","button");
+        spinnerMinus.setAttribute("type","button");
+        spinnerPlus.setAttribute("class","numberPlus");
+        spinnerMinus.setAttribute("class","numberMinus");
+        spinnerPlus.innerHTML = " + ";
+        spinnerMinus.innerHTML = " - ";
+
+		numberSpinner.appendChild(spinnerMinus);
+		spinnerSection.appendChild(spinnerInput);
+		numberSpinner.appendChild(spinnerSection);
+		numberSpinner.appendChild(spinnerPlus);
+        
+		landing.appendChild(numberSpinner);
+
+};
+/*========================================================*/
+/*==================|     Prices      |==================*/
+/*======================================================*/
+
+let createPrices = function(landing){
+
+	let cartPrices = cartConfig.Prices;
+
+	Array.prototype.forEach.call(cartPrices, function(item, index) {
+		let description = item.Description;
+		let grouping = item.Grouping;
+
+		let priceOption = document.createElement('UL');
+		priceOption.setAttribute("class","price-option");
+		priceOption.setAttribute('data-group',grouping.replace(/\s/g, ''));
+
+		let priceSpinnerCont = document.createElement("LI");
+		priceSpinnerCont.setAttribute("class","price-spinner-cont");
+		createSpinners(priceSpinnerCont,item.ControlName,index);
+
+		let priceDescription = document.createElement("LI");
+		priceDescription.setAttribute('class','price-description');
+		priceDescription.innerHTML = "<p>" + description + "</p>";
+
+		let PricePrices = document.createElement("li");
+		PricePrices.setAttribute('class','price-prices');
+		PricePrices.innerHTML = "<p>from<span class='linethru'>$" + 
+								item.ListPrice + "</span></p><p>now<span>$" + 
+								item.Saleprice + "</span></p>";
+
+		priceOption.appendChild(priceSpinnerCont);
+		priceOption.appendChild(priceDescription);
+		priceOption.appendChild(PricePrices);
+
+		landing.appendChild(priceOption);
+	});
+
+};
+/*========================================================*/
+/*==============|  Additional Collectors  |==============*/
+/*======================================================*/
+
+let createAdditionalCollectors = function(landing){
+
+	let collectors = cartConfig.Collectors;
+	collectors = collectors.slice(1,collectors.length);
+
+	//Name
+
+	Array.prototype.forEach.call(collectors, function(item, index) {
+
+
+		let collector = document.createElement("DIV");
+		collector.setAttribute("class","collector");
+
+		let collectorInput;
+
+		if(item.ApplicationDataType === 7){
+
+			collectorInput = document.createElement("SELECT");
+			let dropDown = item.ListMember.ListMembers;
+		
+			dropDown.push("" + item.Name + "");
+
+			Array.prototype.forEach.call(dropDown, function(element,elementIndex){
+				let _option = document.createElement("option");
+				_option.innerHTML = element;
+				collectorInput.appendChild(_option);
+			});
+
+		}else if(item.ApplicationDataType === 1){
+			
+			collectorInput = document.createElement("INPUT");
+			collectorInput.setAttribute("type","checkbox");
+
+		}else{
+
+			collectorInput = document.createElement("INPUT");
+			collectorInput.setAttribute("type","text");
+			collectorInput.setAttribute("placeholder",item.Name);
+
+		};
+
+		collectorInput.setAttribute("name",item.ControlName);
+
+		let collectorLabel = document.createElement("LABEL");
+		collectorLabel.innerHTML = item.Name;
+		collectorLabel.setAttribute("for",item.ControlName);
+
+		collector.appendChild(collectorLabel);
+		collector.appendChild(collectorInput); 
+
+		landing.appendChild(collector);
+	});
+};
+/*========================================================*/
+/*==================|  Create Header  |==================*/
+/*======================================================*/
+
+let createCheckoutHeader = function(){
+	let checkOutHeader = document.getElementById("checkout-header");
+	checkOutHeader.innerHTML = cartConfig.ProductTitle;
+}
+
+/*========================================================*/
+/*==================|  Create page 1  |==================*/
+/*======================================================*/
+
+let createPage1 = function(){
+	createDatePicker(page1.firstElementChild);
+};
+
+/*========================================================*/
+/*==================|  Create page 2  |==================*/
+/*======================================================*/
+
+let createPage2 = function(){
+	createPrices(page2.firstElementChild);
+
+	let numberSpinnerPlus = document.getElementsByClassName("numberPlus");
+	let numberSpinnerMinus = document.getElementsByClassName("numberMinus");
+
+	for (var i = 0; i <  numberSpinnerPlus.length; i++) {
+    	numberSpinnerPlus[i].addEventListener( "click" ,function(){
+        	numIncrement("" + this.previousElementSibling.firstChild.id + "",true)
+    	});
+	};
+
+	for (var i = 0; i < numberSpinnerMinus.length; i++) {
+    	numberSpinnerMinus[i].addEventListener( "click" ,function(){
+        	numIncrement("" + this.nextElementSibling.firstChild.id + "",false)
+    	});
+	};
+
+	let $price = document.getElementsByClassName("price");
+
+	Array.prototype.forEach.call($price, function(item,index) {
+		if(item.value.length < 1){
+			item.value = 0;
+		};
+	});
+};
+
+/*========================================================*/
+/*==================|  Create page 3  |==================*/
+/*======================================================*/
+
+let createPage3 = function(){
+	createAdditionalCollectors(page3.firstElementChild);
+};
+
+/*========================================================*/
+/*==================|     events!     |==================*/
+/*======================================================*/
+
+createCheckoutHeader();
+createPage1();
+createPage2();
+createPage3();
+
+let pageIndex;
+pageIndex = 1;
+
+let showPage1 = function(){
+	page1.className = "addToCartPage active";
+	page2.className = "addToCartPage";
+	page3.className = "addToCartPage";
+	pageIndex = 1;
+};
+let showPage2 = function(){
+	page1.className = "addToCartPage";
+	page2.className = "addToCartPage active";
+	page3.className = "addToCartPage";
+	pageIndex = 2;
+};
+let showPage3 = function(){
+	page1.className = "addToCartPage";
+	page2.className = "addToCartPage";
+	page3.className = "addToCartPage active";
+	pageIndex = 3;
+};
+
+let openCheckout = function(){
+    document.getElementById("checkout-window").className = "active";
+    showPage1();
+};
+let closeCheckout = function(){
+    document.getElementById("checkout-window").className = "";
+};
